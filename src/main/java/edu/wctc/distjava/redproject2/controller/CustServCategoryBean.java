729df1dbcfde87eb14d69ce20f990b4da4872545
Neal <Neal@ThePC>
2013-03-26 15:13:53 -0500

    Customer Service page completed.  Name, email and message fields are required.

---
+++ src/main/java/edu/wctc/distjava/redproject2/controller/CustServCategoryBean.java
@@ -0,0 +1,39 @@
+package edu.wctc.distjava.redproject2.controller;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import javax.inject.Named;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Scope;
+/**
+ *
+ * @author Neal Sorenson
+ */
+
+@Named("csbean")
+@Scope("request")
+public class CustServCategoryBean implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private final Logger LOG = LoggerFactory.getLogger(CustServCategoryBean.class);
+    
+    private List<String> categoryList;
+    
+    public CustServCategoryBean(){
+        categoryList = new ArrayList<String>();
+            categoryList.add("Buying an item");
+            categoryList.add("Selling an item");
+            categoryList.add("Registration");
+            categoryList.add("Technical Issues"); 
+    }
+    
+    public List<String> getCategoryList() {
+        return categoryList;
+    }
+
+    public void setCategoryList(List<String> categoryList) {
+        this.categoryList = categoryList;
+    }
+}
